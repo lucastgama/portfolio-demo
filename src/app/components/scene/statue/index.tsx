aaf5c313d79ed4ee43416470e997a8855fdb930e
lucastgama <lucas_tgama@hotmail.com>
2024-06-04 22:51:58 -0300

    feat: new arg sphere and new color for object plane

--- src/app/components/scene/statue/index.tsx
+++ src/app/components/scene/statue/index.tsx
@@ -8,14 +8,17 @@ import {
   Sphere,
   SpotLight,
 } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { useRef, useState } from "react";
 import { StatueObject } from "../../meshes/statue";
 
 const MouseTracker = ({ setMousePosition }) => {
+  const { viewport } = useThree();
+
   useFrame((state) => {
-    const x = state.pointer.x;
-    const y = state.pointer.y;
+    const x = (state.pointer.x * viewport.width) / 4.2;
+    const y = (state.pointer.y * viewport.height) / 4.2;
+
     setMousePosition({ x, y });
   });
   return null;
@@ -40,20 +43,27 @@ const Statue = () => {
         <PerspectiveCamera makeDefault position={[0, 0, 10]} fov={50} />
         <pointLight position={[1, -1, -2]} intensity={1} />
         <pointLight position={[0, 1, 1]} intensity={0.05} />
+        <pointLight position={[-1, 0, -1]} intensity={0.05} />
+
         <SpotLight
-          angle={0.9}
+          angle={0.8}
           attenuation={0}
-          intensity={10}
-          distance={30}
+          intensity={1.6}
+          distance={10}
           position={[mousePosition.x, mousePosition.y, 3]}
           target={sphereRef.current}
+          penumbra={1}
         />
         <StatueObject />
-        <Plane receiveShadow args={[20, 20]} position={[0, 0, -1]}>
-          <meshStandardMaterial color="hotpink" />
+        <Plane receiveShadow args={[15, 15]} position={[0, 0, -0.5]}>
+          <meshStandardMaterial color="#9d9d9d" />
         </Plane>
-        <Sphere ref={sphereRef} args={[0.5]} position={[mousePosition.x * 4.5, mousePosition.y * 4.5, 0]} />
-        <SoftShadows samples={2} />
+        <Sphere
+          ref={sphereRef}
+          args={[0.001]}
+          position={[mousePosition.x, mousePosition.y, 0]}
+        />
+        <SoftShadows samples={1} />
         <MouseTracker setMousePosition={setMousePosition} />
       </Canvas>
       <div style={{ position: "absolute", top: 10, left: 10, color: "white" }}>
