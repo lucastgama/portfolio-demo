b029dea91b188118679d0dd818de64f8a3889dc8
lucastgama <lucas_tgama@hotmail.com>
2024-06-30 13:28:38 -0300

    fix: Remoção navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "@/app/styles/stylesGlobal.css";
-import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import StyledComponentsRegistry from "./registry";
 
